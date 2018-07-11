a36bfaa8e7ba08b25fd49f9c288fb19d0f15efa6
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-07-11 19:28:32 +0200

    feat(Spotify): support params for getArtistAlbums

--- src/resources/spotify/modules/artists/getArtistAlbums.ts
+++ src/resources/spotify/modules/artists/getArtistAlbums.ts
@@ -1,16 +1,43 @@
-import { request, Request } from '../../request';
+import { request, Request, RequestParams } from '../../request';
 import { Pagination } from '../definitions';
-import { SpotifySimplifiedAlbum } from '../albums';
+import { SpotifySimplifiedAlbum, SpotifyAlbumGroup } from '../albums';
 
 export type GetArtistAlbumsOptions = {
   id: string;
+  include_groups?: SpotifyAlbumGroup[];
+  market?: string; // @WEAK: ISO 3166-1 alpha-2 country codes
+  limit?: number;
+  offset?: number;
 };
 
 type GetArtistAlbumsRequest = Request<GetArtistAlbumsOptions, Pagination<SpotifySimplifiedAlbum>>;
 
-export const getArtistAlbums: GetArtistAlbumsRequest = ({ id }, requestOptions) =>
-  request<Pagination<SpotifySimplifiedAlbum>>({
+export const getArtistAlbums: GetArtistAlbumsRequest = (
+  { id, include_groups, market, limit, offset },
+  requestOptions,
+) => {
+  const params: RequestParams = {};
+
+  if (include_groups) {
+    params.include_groups = include_groups.join(',');
+  }
+
+  if (market) {
+    params.market = market;
+  }
+
+  if (limit) {
+    params.limit = limit;
+  }
+
+  if (offset) {
+    params.offset = offset;
+  }
+
+  return request<Pagination<SpotifySimplifiedAlbum>>({
     ...requestOptions,
     endpoint: `/artists/${id}/albums`,
     method: 'GET',
+    params,
   });
+};
