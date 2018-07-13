a3d4c0d05415a9a58807cd691fc29c454bb3ffc4
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-07-13 18:45:25 +0200

    feat(Spotify): export argument

--- src/resources/spotify/index.ts
+++ src/resources/spotify/index.ts
@@ -3,5 +3,12 @@ export { createSpotifyClient, SpotifyClient } from './client';
 
 // Entity
 export * from './modules/definitions';
-export { SpotifyFullArtist, SpotifySimplifiedArtist } from './modules/artists';
+
+export {
+  GetArtistOptions,
+  GetArtistAlbumsOptions,
+  SpotifyFullArtist,
+  SpotifySimplifiedArtist,
+} from './modules/artists';
+
 export { SpotifySimplifiedAlbum, SpotifyAlbumType } from './modules/albums';
