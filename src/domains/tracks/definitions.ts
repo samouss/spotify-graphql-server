88f36486332c8787c50d65599ebb9332d40ec765
Vaillant Samuel <samuel.vllnt@gmail.com>
2018-08-07 19:46:37 +0200

    feat(domain): create top level types for tracks

---
+++ src/domains/tracks/definitions.ts
@@ -0,0 +1,34 @@
+import { SpotifyExternalIds, SpotifyExternalURLS, SpotifyRestrictions } from '../definitions';
+import { SpotifyGraphQLAlbum } from '../albums';
+import { SpotifyGraphQLArtist } from '../artists';
+
+export type TrackLink = {
+  externalURLs: SpotifyExternalURLS;
+  href: string;
+  id: string;
+  type: string;
+  uri: string;
+};
+
+export type SpotifyTrack = {
+  album: SpotifyGraphQLAlbum;
+  artists: SpotifyGraphQLArtist[];
+  availableMarkets: string[]; // @WEAK: ISO 3166-1 alpha-2 country codes
+  discNumber: number;
+  durationMS: number;
+  explicit: boolean;
+  externalIds: SpotifyExternalIds;
+  externalURLs: SpotifyExternalURLS;
+  href: string;
+  id: string;
+  isPlayable: boolean;
+  linkedFrom: TrackLink;
+  restrictions: SpotifyRestrictions;
+  name: string;
+  popularity: number;
+  previewURL: string | null;
+  trackNumber: number;
+  type: 'track';
+  uri: string;
+  isLocal: boolean;
+};
